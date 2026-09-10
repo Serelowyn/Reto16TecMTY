72438e750ccce75a693b01261b51bb7dcb45abd7
Memo <fretn655@gmail.com>
2026-09-09 18:36:59 -0600

    se hacen algunas revisiones para conocer mas a fondo el df de empleados, y se encuentra que cada una de las columnas tienen caracteres con mayusculas. se realiza una pequeña modificacion, haciendo que sean todas en minusculas para estandarizar el df

--- DS_C6_SC2_LuisGuillermoTorresMagdaleno.py
+++ DS_C6_SC2_LuisGuillermoTorresMagdaleno.py
@@ -8,6 +8,21 @@ import pandas as pd
 
 empleados = pd.read_csv("Train.csv")
 
+# a continuacion se realizaran algunas pruebas para poder conocer la forma del df general, y ademas de tipos de datos y ver si se necesitan cambios adicionales
+
+empleados.shape
+empleados.dtypes
+empleados.info()
+empleados.columns
+"""aqui se identificaron mayusculas en todos los nombres de columnas, entonces lo limpio para seguir"""
+
+empleados.columns = empleados.columns.str.lower()
+empleados.columns
+
+"""esto me sirve para ver la cantidad de celdas con faltantes, en caso de que se requiera despues"""
+empleados.isna().sum()
+
+empleados.head()
 
 # ----------------------- 
 # ----------------------- 
